3bb7ead64e9e529982cfe4e42920d0420956963e
Nila008 <82997083+Nila008@users.noreply.github.com>
2021-06-19 15:23:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -21,30 +21,30 @@ async def on_ready():
     print("https://discord.gg/cKQGjsS")
     print("Everything's all ready to go~")
     while True:
-    	await bot.change_presence(activity=discord.Streaming(name="Selling Nitro/token gen", url="https://www.twitch.tv"))
-    	await asyncio.sleep(2)
+    	await bot.change_presence(activity=discord.Streaming(name="SELLING NITRO/TOKEN GEN", url=~"https://www.twitch.tv"))
+    	await asyncio.sleep(5)
     	
-    	await bot.change_presence(activity=discord.Streaming(name="Made by Nila", url="https://www.twitch.tv"))
-    	await asyncio.sleep(2)
+    	await bot.change_presence(activity=discord.Streaming(name="IQ 10/10", url=~"https://www.twitch.tv"))
+    	await asyncio.sleep(5)
 	
-    	await bot.change_presence(activity=discord.Game(name="discord.gg/xlop"))
-    	await asyncio.sleep(2)
+    	await bot.change_presence(activity=discord.Streaming(name="MADE BY NILA", url=~"https://www.twitch.tv"))
+    	await asyncio.sleep(5)
     	
-    	await bot.change_presence(activity=discord.Streaming(name="CASH", url="https://www.twitch.tv"))
-    	await asyncio.sleep(2)
+    	await bot.change_presence(activity=discord.Game(name="discord.gg/xlop", url=~"https://www.twitch.tv"))
+    	await asyncio.sleep(5)
     	
-    	await bot.change_presence(activity=discord.Streaming(name="JOIN XL NOW 25K OP FAM", url="https://www.twitch.tv"))
-    	await asyncio.sleep(2)
+    	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="24/7"))
+    	await asyncio.sleep(5)
     	
-    	await bot.change_presence(activity=discord.Streaming(name="selling this", url="https://www.twitch.tv"))
-    	await asyncio.sleep(2)
+    	await bot.change_presence(activity=discord.Game(name="PAYTM CASH"))
+    	await asyncio.sleep(5)
     	
-    	await bot.change_presence(activity=discord.Streaming(name="PEACE", url="https://www.twitch.tv"))
-    	await asyncio.sleep(2)
+    	await bot.change_presence(activity=discord.Activity(type=1,name="NILA OP"))
+    	await asyncio.sleep(5)
     	
     
     	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'''{len(bot.guilds)} servers'''))
-    	await asyncio.sleep(2)
+    	await asyncio.sleep(5)
 
 @bot.command(name="hi")
 async def ping(ctx):
@@ -57,4 +57,4 @@ async def ping(ctx):
     # Send it to the user
     await ctx.send(latency)
     
-bot.run("mfa.0Aij4oZ9iF8852Yy8e96dz9pTz7lPk0AawelMMib8tYFtliPn2TEfWfme9pF_vVi2E1gD5a6Gfpdpm70PrcA",bot=False)
+bot.run("NzA0ODg1NzgxOTE0ODQ1MjQ2.YDzFpA.ZVGamNEabRBev0EyQ0umxnu2o3o",bot=False)
